8f92fdc972656e06faed875d0fbcc5c30b4efad5
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-09-07 20:18:23 +0200

    Removed card rendering code to another file

--- js/recipe.js
+++ js/recipe.js
@@ -1,4 +1,5 @@
 import { renderError } from "./utils/error.js";
+import { createRecipeCard } from "./utils/createRecipeCard.js";
 
 const mainContent = document.getElementById("main");
 const recipeContainer = document.getElementById("healthy-recipes-container");
@@ -15,53 +16,8 @@ async function loadRecipes() {
 
 
     data.forEach(item => {
-      const card = document.createElement("div");
-      card.className = "recipe-card";
-      const recipeImg = document.createElement("picture");
-      const sourceLarge = document.createElement("source");
-      sourceLarge.srcset = item.image.large;
-      sourceLarge.media = "(min-width: 48em)";
-      const sourceSmall = document.createElement("source");
-      sourceSmall.srcset = item.image.small;
-      sourceSmall.media = "(max-width: 47.99em)";
-      const fallbackImg = document.createElement("img");
-      fallbackImg.src = item.image.small;
-      fallbackImg.alt = item.slug;
-      const recipeTitle = document.createElement("h2");
-      recipeTitle.textContent = item.title;
-      const recipeDesc = document.createElement("p");
-      recipeDesc.textContent = item.overview;
-      const recipeIcons = document.createElement("div");
-      recipeIcons.className = "recipe-card-icons";
-      const firstIconContainer = document.createElement("span");
-      const firstIconImg = document.createElement("img");
-      firstIconContainer.textContent = `Servings: ${item.servings}`;
-      firstIconImg.src = "../assets/images/icon-servings.svg";
-      firstIconImg.alt = "servings icon";
-      const secondIconContainer = document.createElement("span");
-      const secondIconImg = document.createElement("img");
-      secondIconContainer.textContent = `Prep: ${item.prepMinutes} min`;
-      secondIconImg.src = "../assets/images/icon-prep-time.svg";
-      secondIconImg.alt = "prep icon";
-      const thirdIconContainer = document.createElement("span");
-      const thirdIconImg = document.createElement("img");
-      thirdIconContainer.className = "cook-icon";
-      thirdIconImg.src = "../assets/images/icon-cook-time.svg";
-      thirdIconImg.alt = "cook time icon";
-      thirdIconContainer.textContent = `Cook: ${item.cookMinutes} min`;
-      const recipeButton = document.createElement("a");
-      recipeButton.href = `details.html?id=${item.id}`;
-      recipeButton.className = "view-recipe-btn";
-      recipeButton.textContent = "View Recipe";
-
-
-      recipeImg.append(sourceLarge, sourceSmall, fallbackImg);
-      firstIconContainer.appendChild(firstIconImg);
-      secondIconContainer.appendChild(secondIconImg);
-      thirdIconContainer.appendChild(thirdIconImg);
-      recipeIcons.append(firstIconContainer, secondIconContainer)
-      card.append(recipeImg, recipeTitle, recipeDesc, recipeIcons, thirdIconContainer, recipeButton);
-      recipeContainer.appendChild(card)
+      const card = createRecipeCard(item);
+      recipeContainer.appendChild(card);
     })
 
   } catch (err) {
